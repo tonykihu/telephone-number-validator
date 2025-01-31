91cef9665fed3d91889726e5721ae0b334a28c46
tonykihu <{wamuthumbikihu@gmail.com}>
2025-01-31 17:55:08 +0300

    TNV

--- script.js
+++ script.js
@@ -2,20 +2,39 @@ const userInput = document.getElementById('user-input');
 const checkButton = document.getElementById('check-btn');
 const clearButton = document.getElementById('clear-btn');
 const result = document.getElementById('results-div');
+const phoneRegex = /^1?\s?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+function resultStyle() {
+    result.style.fontSize = "2em";
+    result.style.fontWeight = "bold";
+    result.style.textAlign = "center";
+    result.style.marginTop = "1rem";
+    result.style.backgroundColor = "#fff8dc";
+    result.style.padding = "20px";
+    result.style.borderRadius = "10px";
+    result.style.boxShadow = "0 0 10px 0 #000";
+    result.style.width = "50%";
+    result.style.margin = "auto";
+}
 
 checkButton.addEventListener('click', () => {
     const userInputValue = userInput.value;
-    const regex = /^1?\s?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
-    if (userInputValue.match(regex) = "") {
+    result.style.display = "block";
+    if (userInputValue.trim() === "") {
         alert("Please provide a phone number");
-    } else if (regex.test(userInputValue)) {
+    } else if (phoneRegex.test(userInputValue)) {
         result.textContent = `Valid US number: ${userInputValue}`;
+        result.style.color = "green";
+        resultStyle();
     } else {
         result.textContent = `Invalid US Number: ${userInputValue}`;
+        result.style.color = "red";
+        resultStyle();
     }
 });
 
 clearButton.addEventListener('click', () => {
     userInput.value = "";
     result.textContent = "";
+    result.style.display = "none";
 });
